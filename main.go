7a9768ad20ee1a33174bc0c1e95420cb8841d01e
Heath Paddock <hp@heathpaddock.com>
2018-05-25 15:46:52 -0500

    modifications for garage-door

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
 	broker := flag.String("b", "", "Specify an mqtt broker (tcp://mqtt-broker-host.foobar.com:1883)")
 	flag.Parse()
 
-	pins := []uint{4, 17, 27} // GPIO pin numbers, NOT the position on the raspberry pi
+	pins := []uint{26} // GPIO pin numbers, NOT the position on the raspberry pi
 	channel := debounce(watchPins(pins))
 
 	mqtt := MqttClient{Broker: *broker}
@@ -20,9 +20,13 @@ func main() {
 	for {
 		select {
 		case e := <-channel:
-			fmt.Println(e)
-			fmt.Println(fmt.Sprintf("pin/%d", e.Pin))
-			mqtt.publish(fmt.Sprintf("pin/%d", e.Pin), fmt.Sprintf("%d", e.Value))
+			if e.Value == 0 {
+				fmt.Println("closed")
+				mqtt.publish("garage/door/status", "closed")
+			} else {
+				fmt.Println("open")
+				mqtt.publish("garage/door/status", "open")
+			}
 		}
 	}
 }
