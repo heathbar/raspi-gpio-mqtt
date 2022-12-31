59971b93bf481cdf8309bc2a3af9cc9aad0b5e98
Heath Paddock <hp@heathpaddock.com>
2022-12-31 14:25:56 -0600

    add support for username & password

--- main.go
+++ main.go
@@ -8,12 +8,14 @@ import (
 func main() {
 
 	broker := flag.String("b", "", "Specify an mqtt broker (tcp://mqtt-broker-host.foobar.com:1883)")
+	user := flag.String("u", "", "Specify an mqtt user")
+	pass := flag.String("p", "", "Specify an mqtt password")
 	flag.Parse()
 
-	pins := []uint{4, 17, 27} // GPIO pin numbers, NOT the position on the raspberry pi
+	pins := []uint{26} // GPIO pin numbers, NOT the position on the raspberry pi
 	channel := debounce(watchPins(pins))
 
-	mqtt := MqttClient{Broker: *broker}
+	mqtt := MqttClient{Broker: *broker, User: *user, Pass: *pass}
 	mqtt.connect("raspi-gpio-client")
 
 	fmt.Println("Ready...")
